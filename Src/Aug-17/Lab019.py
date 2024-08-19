5e2c24f7af716a24ea0ef7473e4e8d506281b204
Shylaja <shylaja.sk66@gmail.com>
2024-08-19 22:34:46 +0530

    All the files of August 13th

---
+++ Src/Aug-17/Lab019.py
@@ -0,0 +1,5 @@
+#Literals
+#variable_namne=variable_name
+#Python is dynamic types of launguage -- Literals value are determine the data type
+age = 10 #int
+pi = 3.14
---
+++ Src/Aug-17/Lab020.py
@@ -0,0 +1,15 @@
+#Escape Sequence
+print("Hello world")
+print("Hello\nworld")
+print("Hello\tworld")
+print("Hello\bworld")
+
+
+#Concept of r (Raw String)
+dir = r'C:\world\n.txt'
+dir2 = r"C:\world\n.txt"
+print(dir)
+print(dir2)
+
+print("Hello\'world")
+
