c2578567f4a09a4fd2c0582d0bb0acb0d2c0defc
Shylaja <shylaja.sk66@gmail.com>
2024-08-20 20:19:22 +0530

    All the files of August 17th

---
+++ Src/Aug-17/Lab021.py
@@ -0,0 +1,37 @@
+#Operator
+
+age = 20
+print(age+1)
+print(20+2)
+
+# -- assignment operator (to store the value)
+#divmod()
+# // give quotient
+# / normal division
+
+# ** power
+
+
+
+print(4//2)
+print(2 == 3)
+print(3==3)
+
+#not operator
+print(not "is_car_repaiered")
+
+#Comparision/logical operator
+# > < >=, <=
+a = 10
+b = 20
+print(a > b)
+print(a<b)
+
+
+
+# Or amd and gate
+a = True
+b = False
+print(a or b)
+print (a and b)
+
